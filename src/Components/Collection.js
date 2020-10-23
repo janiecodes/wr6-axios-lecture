5f22a7115f1b103210f9460666fa2676c44a5943
Janie Kim <janiekim@Janies-MBP.attlocal.net>
2020-10-23 11:54:12 -0500

    saving point

--- src/Components/Collection.js
+++ src/Components/Collection.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Card from './Card';
+import axios from '.axios';
 
 class Collection extends Component {
     constructor(){
@@ -9,9 +10,23 @@ class Collection extends Component {
         }
     }
 
+    componentDidMount(){
+        this.getCards()
+    }
+
+    getCards(){
+        axios.get("https://api.pokemontcg.io/v1/cards")
+        .then(res => {
+            this.setState({
+                cards: res.data.cards
+            })
+        }).catch(err => console.log(err))
+    }
+
     render(){
+        console.log("STATE: ", this.state)
         const mappedCards = this.state.cards.map( card => {
-            return <Card/>
+            return <Card key={card.id} name={card.name} img={card.imageUrl}/>
         })
         return(
             <div className="collection">
